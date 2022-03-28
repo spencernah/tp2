6a2b8242beb317772aed6dda728eb39e0fffd43e
DESKTOP-3U485CE\User <spencernzh@gmail.com>
2022-03-28 18:56:52 +0800

    checkstyle

--- src/test/java/seedu/address/logic/parser/FindRemarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindRemarkCommandParserTest.java
@@ -17,7 +17,8 @@ public class FindRemarkCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindRemarkCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                FindRemarkCommand.MESSAGE_USAGE));
     }
 
     @Test
