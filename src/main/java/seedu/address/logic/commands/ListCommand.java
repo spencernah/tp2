27012ebda5228690bd5bfbada771c8ebe4f23ce7
IcyNoPeople <leowjunhui2@hotmail.com>
2022-03-30 21:47:45 +0800

    List number of person

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 
 /**
@@ -12,13 +13,17 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_SUCCESS = "Listed all persons (You have %1$d contacts)";
+    public static final String MESSAGE_NO_CONTACT = "Listed all persons (You have do not have any contacts)";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        if(model.getFilteredPersonList().size() == 0){
+            return new CommandResult(MESSAGE_NO_CONTACT);
+        }
+        return new CommandResult(String.format(MESSAGE_SUCCESS, model.getFilteredPersonList().size()));
     }
 }
